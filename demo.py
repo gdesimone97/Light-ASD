610ef35896b268ffd8298c1dfa0960095e9c19e3
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-05-19 11:35:34 +0200

    ok

---
+++ demo.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+import torch
+from pathlib import Path
+from model.faceDetector import S3FD
+from model.Model import ASD_Model
+from collections import OrderedDict
+
+if __name__ == "__main__":
+    print("Cuda available:", torch.cuda.is_available())
+    curr_dir = Path(__file__).parent
+
+    face_dectecor = S3FD()
+
+    model_path = curr_dir.joinpath("weight", "pretrain_AVA_CVPR.model")
+    weights = torch.load(model_path)
+
+    weights_nw = OrderedDict()
+    for k in weights.keys():
+        try:
+            k_nw = k.split("model.")[1]
+            weights_nw[k_nw] = weights[k]
+        except IndexError:
+            continue
+    
+    del weights
+
+    model = ASD_Model()
+    model.load_state_dict(weights_nw)
+    model.eval()
+
+
+    cap = cv.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            bboxes = face_dectecor.detect_faces(frame)
+            for bbox in bboxes:
+                x, y, w, h, _ = bbox
+                cv.rectangle(frame, [int(x), int(y)], [int(w), int(h)], (255, 0, 0))
+            cv.imshow("Demo", frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv.destroyAllWindows()
